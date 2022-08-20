1bfb32858bf23ed5f44bb3d83e8fe54b0b04da2d
Yutthana Yahan <thyahan@gmail.com>
2022-08-21 00:22:11 +0700

    feat: :sparkles: implement pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>SSR Boilerplate</title>
-        <meta name="description" content="SSR boilerplate" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="w-screen h-screen flex justify-center items-center bg-slate-600 text-white">
-        <h1 className="text-6xl">Hello</h1>
+        <h1 className="text-6xl">Where is my boy friend</h1>
       </main>
     </div>
   );
